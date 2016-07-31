2c84a358182c5d385e885b91fb4e5997b5cb9dcc
Mac <Zhangping@172-13-2-153.lightspeed.mssnks.sbcglobal.net>
2016-07-31 16:40:21 +0800

    修改

--- StarRatingView/AppDelegate.m
+++ StarRatingView/AppDelegate.m
@@ -19,6 +19,7 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
 }
