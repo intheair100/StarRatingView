b93b80b425a6d8becd07174c2f5e6570f0111919
Mac <Zhangping@androidxxx.local>
2016-07-31 16:33:52 +0800

    Initial Commit

---
+++ StarRatingView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StarRatingView
+//
+//  Created by Mac on 16/7/31.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
